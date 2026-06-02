2704fc22f27b6762e7ac850488d2848d298e15b3
Phuree Plubjui <pplubphuree@gmail.com>
2026-06-03 04:12:01 +0700

    Revert "Update main.py"
    This reverts commit c7a74e846cad8bfb5734543ad15fbdb8498433e0.

--- main.py
+++ main.py
@@ -1,52 +1,63 @@
 import cv2 as cv
 import numpy as np
 import os
+import keyboard
 from time import time
 from windowcapture import WindowCapture
-from wafer_detector import WaferDetector
+from waferdetector import WaferDetector
 from notification import WindowNotify
 
+
+
+# Change the working directory to the folder this script is in.
+# Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-wincap   = WindowCapture('RealVNC')
+
+# initialize the WindowCapture class
+# wincap = WindowCapture('window name')
+wincap = WindowCapture('RealVNC Viewer')    # program's name is focused on
 detector = WaferDetector()
-notify   = WindowNotify("Prober Screen")
+notify = WindowNotify("Prober Screen")      # topic of notification
+
 notify.started()
 
+
 loop_time = time()
-while True:
-    try:
-        # Get screenshot once per frame
-        screenshot = wincap.get_screenshot()
+while(True):
 
-        # Find black area with circle shape inside
-        wafer_screenshot, wafer_pos, circle_info = detector.find_wafer(screenshot)
+    # if keyboard.is_pressed('esc'):
+    #     notify.stopped()
+    #     break
 
-        if wafer_screenshot is not None:
-            # Draw box on full screenshot
-            screenshot = detector.draw_wafer_box(screenshot, wafer_pos)
+    # get an updated image of the program
+    screenshot = wincap.get_screenshot()
 
-            # Draw circle on wafer image
-            wafer_with_circle = detector.draw_circle(
-                                    wafer_screenshot.copy(), 
-                                    circle_info)
+    wafer_screenshot, wafer_pos = detector.find_wafer(screenshot)
 
-            # Show wafer map with circle drawn
-            cv.imshow('Wafer Map', wafer_with_circle)
-        else:
-            print("Wafer not found!")
+    if wafer_screenshot is not None:
+        # draw box on full screenshot
+        screenshot = detector.draw_wafer_box(screenshot, wafer_pos)
 
-        cv.imshow('Computer Vision', screenshot)
+        # show wafer map
+        cv.imshow('Wafer Map', wafer_screenshot)
+    else:
+        print("Wafer not found!")
 
-        fps = 1 / (time() - loop_time)
-        print(f'FPS: {fps:.1f}')
-        loop_time = time()
 
-    except Exception as e:
-        print(f'Error: {e}')
-        break
+    # display the image
+    cv.imshow('Computer Vision', screenshot)
 
+    # detector.show_mask(screenshot)
+
+    # debug the loop rate
+    # print('FPS {}'.format(1 / (time() - loop_time)))
+    # loop_time = time()
+
+    # # press 'q' with the output window focused to exit.
+    # # waits 1 ms every loop to process key presses
     key = cv.waitKey(1)
+
     if key == ord('q'):
         cv.destroyAllWindows()
         notify.stopped()
